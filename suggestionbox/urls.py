c4e72668aad0f4a8d27db4401e968d90edc00ac3
Muhammad Fahreza Azka Arafat <beyond2lte@gmail.com>
2022-10-28 19:47:35 +0700

    Adding user feedback module.

---
+++ suggestionbox/urls.py
@@ -0,0 +1,14 @@
+import suggestionbox.views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'suggestionbox'
+
+urlpatterns = [
+    path('', suggestionbox.views.showFeedback, name='showFeedback'),
+    path('giveFeedback/', suggestionbox.views.giveFeedback, name='giveFeedback'),
+    path('replyFeedback/<str:id>/', suggestionbox.views.replyFeedback, name='replyFeedback'),
+    path('showJson/', suggestionbox.views.showJson, name='showJson'),
+]
+urlpatterns  += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
