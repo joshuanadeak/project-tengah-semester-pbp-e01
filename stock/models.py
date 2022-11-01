5851afc9302320ba61cacf195daffe2df7a3610c
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2022-11-01 09:17:06 +0700

    coba app stock

--- stock/models.py
+++ stock/models.py
@@ -1,9 +1,22 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Saham(models.Model):
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
-    kode_saham = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    kode_saham = models.CharField(max_length=10, default="ABCD")
+    nama_perusahaan = models.TextField(default="PT ABCD Tbk.")
     harga_saham = models.IntegerField()
-    risk = models.TextField()
+    risk = models.CharField(max_length=10)
+
+class Pasar(models.Model):
+    kode_saham = models.CharField(max_length=10, default="ABCD")
+    nama_perusahaan = models.TextField(default="PT ABCD Tbk.")
+    harga_saham = models.IntegerField()
+    risk = models.CharField(max_length=10)
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    catatan = models.TextField(default="jangan monopoli saham yaa")
