d333d6cea5533827800498cea3b7cedce04173d6
puddingoverdose <ridho050302@gmail.com>
2022-11-02 19:16:45 +0700

    Updated models.py

--- registration/models.py
+++ registration/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
-class UserFeedback(models.Model):
-    feedback = models.TextField()
-    reply = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    username = models.CharField(max_length=100, null=True, blank=True)
+class CompanyRegistration(models.Model):
+    name = models.CharField()
+    price_of_stock = models.IntegerField(validators = [MinValueValidator(0)])
